98b252de383043cd661ead80a653b2ca6b232293
Lakshmi Narasimhan <lakshmi@lakshminp.com>
2018-10-28 21:05:24 -0400

    Working version of custom TF runner image #1

--- estate/settings/estate.py
+++ estate/settings/estate.py
@@ -5,7 +5,7 @@ from . import INSTALLED_APPS
 TERRAFORM_DOCKER_IMAGE = os.environ.get("TERRAFORM_DOCKER_IMAGE", "underarmourconnectedfitness/estate:master")
 
 TERRAFORM_EXTRA_ARGS = os.environ.get("TERRAFORM_EXTRA_ARGS", "-input=false")
-TERRAFORM_INIT_EXTRA_ARGS = os.environ.get("TERRAFORM_INIT_EXTRA_ARGS", "")
+TERRAFORM_INIT_EXTRA_ARGS = os.environ.get("TERRAFORM_INIT_EXTRA_ARGS", "-plugin-dir=/plugins")
 TERRAFORM_PLAN_EXTRA_ARGS = os.environ.get("TERRAFORM_PLAN_EXTRA_ARGS", "-detailed-exitcode -out=plan")
 TERRAFORM_APPLY_EXTRA_ARGS = os.environ.get("TERRAFORM_APPLY_EXTRA_ARGS", "")
 
